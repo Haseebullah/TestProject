4c0889ddbb163c1d629fc7b86f20bbc68c4ec43d
Haseebullah <zindarod@gmail.com>
2014-02-04 11:17:35 +0500

    Add TestProject.cu

--- TestProject.cu
+++ TestProject.cu
@@ -1,16 +1,45 @@
-#include<stdio.h>
+#include<iostream>
 #include<cuda.h>
+#include<stdio.h>
+
+using namespace std;
 
 __global__
-void Test()
+void Test(int* array_d, const int size)
 {
-	printf("Kernel");
+	int id=threadIdx.y*blockDim.x+threadIdx.x;
+
+	if(id>=size)
+		return;
+
+	printf("%d\n",array_d[id]);
 }
 
 
 int main()
 {
+	const int size=1000;
+
+	int array[size];
+
+	int* array_d;
+
+	cudaMalloc((void**)&array_d, sizeof(int)*size);
+
+	for(int i=0;i<size;i++)
+	{
+		array[i]=i;
+	}
 
 
-	Test<<<1,1>>>();
+	cudaMemcpy(array_d,array,sizeof(int)*size,cudaMemcpyHostToDevice);
+
+	dim3 gridblock(1,1,1);
+	dim3 threadsperblock(32,32,1);
+
+
+
+	Test<<<gridblock,threadsperblock>>>(array_d, size);
+	cudaDeviceSynchronize();
+
 }
